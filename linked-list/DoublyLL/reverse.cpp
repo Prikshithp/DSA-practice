9e188d17695b0a5032e1971254d1a73ebd556a31
Prikshit Sharma <153969074+Prikshithp@users.noreply.github.com>
2024-01-03 22:30:17 +0530

    Create reverse.cpp

---
+++ linked-list/DoublyLL/reverse.cpp
@@ -0,0 +1,16 @@
+Node* reverseDLL(Node* head)
+{   
+    // Write your code here 
+    if(head==NULL || head->next ==NULL ){
+        return head;
+    } 
+    Node* prev=NULL;
+    Node* curr=head;
+    while(curr!=NULL){
+        prev=curr->prev;
+        curr->prev=curr->next;
+        curr->next=prev;
+        curr=curr->prev;
+    } 
+    return prev->prev;
+}
